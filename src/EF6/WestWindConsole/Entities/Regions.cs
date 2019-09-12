a2e88c431fdd097d0cc4ae836a53b43a09777807
Janne Abdala <jabdala1@nait.ca>
2019-09-12 11:37:46 -0600

    Added and finished Regions and started Territories class

---
+++ src/EF6/WestWindConsole/Entities/Regions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WestWindConsole.Entities
+{
+    [Table("Regions")]
+    class Regions
+    {
+        // Primary Key
+        [Required, Key]
+        public int RegionID { get; set; }
+
+        public string RegionDescription { get; set; }
+    }
+}
